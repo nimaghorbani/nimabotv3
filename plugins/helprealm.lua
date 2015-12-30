f6bb37a974bc8aacee253412eb7f98aa58efd29d
nimaghorbani <nimaghorbani@users.noreply.github.com>
2015-12-30 08:13:53 -0800

    Update helprealm.lua

--- plugins/helprealm.lua
+++ plugins/helprealm.lua
@@ -1,6 +1,6 @@
 do
     function run(msg, matches)
-	return 'Hi Welcome To GhBot \n For Add Bot To Group : \n Send PM To @MrGhor \n For CreateGroup : \n Send PM To @MrGhor \n For More Information : \n Send Pm To @MrGhor \n 15 Day Free Bot  Creator: @HaWkerGh'
+	return 'Hi Welcome To GhBot \n For Add Bot To Group : \n Send PM To @MrGhor \n For CreateGroup : \n Send PM To @MrGhor \n For More Information : \n Send Pm To @MrGhor \n 15 Day Free Bot \n Creator: @HaWkerGh'
 end
 return {
   patterns = {
